190cbb1c7f7526da2a9f64ce3e2b6a5fafcb2b09
陈安一 <chenay@huntor.cn>
2016-11-14 18:20:07 +0800

    [+]Init Project

---
+++ src/pages/Test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="test">
+    123123123
+  </div>
+</template>
+<style>
+</style>
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  route: {
+    data () {
+      this.$root.state = true;
+    }
+  },
+  methods: {
+  }
+}
+</script>
